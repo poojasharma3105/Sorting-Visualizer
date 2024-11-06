40e41f21b83a807711686c5de1cd085720baa1a7
poojasharma <poojash3105@gmail.com>
2024-11-06 14:22:39 +0530

    latest commit

---
+++ scripts/main.js
@@ -0,0 +1,81 @@
+var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
+var inp_gen=document.getElementById("a_generate");
+var inp_aspeed=document.getElementById("a_speed");
+//var array_speed=document.getElementById('a_speed').value;
+
+var butts_algos=document.querySelectorAll(".algos button");
+
+var div_sizes=[];
+var divs=[];
+var margin_size;
+var cont=document.getElementById("array_container");
+cont.style="flex-direction:row";
+
+//Array generation and updation.
+
+inp_gen.addEventListener("click",generate_array);
+inp_as.addEventListener("input",update_array_size);
+
+function generate_array()
+{
+    cont.innerHTML="";
+
+    for(var i=0;i<array_size;i++)
+    {
+        div_sizes[i]=Math.floor(Math.random() * 0.5*(inp_as.max - inp_as.min) ) + 10;
+        divs[i]=document.createElement("div");
+        cont.appendChild(divs[i]);
+        margin_size=0.1;
+        divs[i].style = `margin: 0% ${margin_size}%; background-color: blue; width: ${100 / array_size - (2 * margin_size)}%; height: ${div_sizes[i]}%;`;
+
+    }
+}
+
+function update_array_size()
+{
+    array_size=inp_as.value;
+    generate_array();
+}
+
+window.onload = function() {
+    update_array_size();  // Call it after page load to initialize the array
+};
+
+//Running the appropriate algorithm.
+for(var i=0;i<butts_algos.length;i++)
+{
+    butts_algos[i].addEventListener("click",runalgo);
+}
+
+function disable_buttons() {
+    for (var i = 0; i < butts_algos.length; i++) {
+        if (butts_algos[i] !== this) {
+            butts_algos[i].disabled = true;
+        }
+    }
+    inp_as.disabled = true;
+    inp_gen.disabled = true;
+    inp_aspeed.disabled = true;
+}
+
+function runalgo() {
+    disable_buttons.call(this);  // Ensure `this` refers to the clicked button
+    this.classList.add("butt_selected");
+    switch (this.innerHTML) {
+        case "Bubble":
+            Bubble();
+            break;
+        case "Insertion":
+            Insertion();
+            break;
+        case "Merge":
+            Merge();
+            break;
+        case "Quick":
+            Quick();
+            break;
+        case "Heap":
+            Heap();
+            break;
+    }
+}
